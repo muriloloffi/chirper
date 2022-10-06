08f143c8c66087794f827e6ea9f0713ac7c6c5b6
Murilo Loffi <murilo.loffi@sc.senai.br>
2022-10-06 12:10:24 -0300

    [chirp] controller, relations done; view WIP

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -10,5 +10,24 @@
             <x-input-error :messages="$errors->get('message')" class="mt-2"></x-input-error>
             <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
         </form>
+
+        <div class="">
+            @foreach($chirps as $chirp)
+                <div class="">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                    </svg>
+                    <div class="">
+                        <div class="">
+                            <div>
+                                <span class="">{{ $chirp->user->name }}</span>
+                                <small class=""> {{ $chirp->created_at->format('j M Y, g:i a') }} </small>
+                            </div>
+                        </div>
+                        <p class=""> {{ $chirp->message }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 </x-app-layout>
