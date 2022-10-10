605839be8c5efb16a207b629989b55a553565565
Murilo Loffi <murilo.loffi@sc.senai.br>
2022-10-10 19:30:58 -0300

    [chirp] destroy chirp done;

--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Models\Chirp;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class ChirpPolicy
 {
@@ -13,8 +14,8 @@ class ChirpPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
     public function viewAny(User $user)
     {
@@ -24,9 +25,9 @@ class ChirpPolicy
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User  $user
+     * @param Chirp $chirp
+     * @return Response|bool
      */
     public function view(User $user, Chirp $chirp)
     {
@@ -36,8 +37,8 @@ class ChirpPolicy
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
     public function create(User $user)
     {
@@ -47,9 +48,9 @@ class ChirpPolicy
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User  $user
+     * @param Chirp $chirp
+     * @return Response|bool
      */
     public function update(User $user, Chirp $chirp)
     {
@@ -59,21 +60,21 @@ class ChirpPolicy
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User  $user
+     * @param Chirp $chirp
+     * @return Response|bool
      */
-    public function delete(User $user, Chirp $chirp)
+    public function delete(User $user, Chirp $chirp): Response|bool
     {
-        //
+        return $this->update($user, $chirp);
     }
 
     /**
      * Determine whether the user can restore the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User  $user
+     * @param Chirp $chirp
+     * @return Response|bool
      */
     public function restore(User $user, Chirp $chirp)
     {
@@ -83,9 +84,9 @@ class ChirpPolicy
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User  $user
+     * @param Chirp $chirp
+     * @return Response|bool
      */
     public function forceDelete(User $user, Chirp $chirp)
     {
